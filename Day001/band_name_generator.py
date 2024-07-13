845b87870b1de50ceefd4eee3ab1a22d96ed1402
Eisi <philipp.eisenmann@googlemail.com>
2024-07-13 16:07:01 +0200

    project 1

---
+++ Day001/band_name_generator.py
@@ -0,0 +1,4 @@
+print("Wrlcome to the band name generator")
+city = input("What's the anme of the city you grew up in?\n")
+animal = input("What's your pet's name?\n")
+print("Your band name could be " + city + " " + animal)
