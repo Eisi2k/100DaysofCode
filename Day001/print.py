bf38fdae23c3ee3da414b83f50641c438fef5f72
Eisi <philipp.eisenmann@googlemail.com>
2024-07-13 12:52:49 +0200

    20240713

---
+++ Day001/print.py
@@ -0,0 +1,13 @@
+print("She said: "'Hello'" an then left." )
+print('She said: "Hello" and then left.')
+
+print("She said: \"Hello\" and then left")
+
+
+print("Hello World!\nHello World!\n\tHello World!")
+print("Hello" + "Philipp")
+print("Hello " + "Philipp")
+print("Hello" + " " + "Philipp")
+
+
+print("Hello" + input("What ist your name? ") + "!")
