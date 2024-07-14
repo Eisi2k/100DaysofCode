48c137d689402ab3d1d276b7446b372e15ebaec0
Eisi <philipp.eisenmann@googlemail.com>
2024-07-14 12:52:28 +0200

    day 3 excercises

---
+++ Day003/multiple_if.py
@@ -0,0 +1,29 @@
+# multiplse of statements
+
+# nested statements
+
+print("Welcome to the rollercoaster!")
+bill = 0
+height = int(input("What is your height in cm? "))
+
+if height >= 120:
+    print("You can ride the rollercoaster!")
+    age = int(input("What is your age? "))
+    if age <= 12:
+        bill = 5
+        print("Child tieckts are $5.")
+    elif age <= 18:
+        bill = 7
+        print("Youth tickets are $7.")
+    else:
+        bill = 12
+        print("Adulsts tickets are $12.")
+
+    wants_photo = input("Do you want a photo taken? Y or N. ")
+    if wants_photo == "Y":
+        #Add $3 to tehr bill
+        bill += 3
+    
+    print(f"Your final bill is {bill}")
+else:
+    print("Sorry, you hvae to grow taller before you can ride.")
