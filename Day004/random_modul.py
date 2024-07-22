ef940ef55cb9a011fe1b34e34a944e0c1755b01a
Eisi <philipp.eisenmann@googlemail.com>
2024-07-22 15:28:13 +0200

    day 4

---
+++ Day004/random_modul.py
@@ -0,0 +1,12 @@
+import random
+
+random_integer = random.randint(1,10)
+print(random_integer)
+
+
+random_float = random.random()
+print(random_float)
+
+
+random_float = random.random()
+print(random_float*5)
