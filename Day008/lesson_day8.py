fc7ca5cf90f9c5c3943e9ff03d2150a65ea9adb8
Eisi <philipp.eisenmann@googlemail.com>
2024-08-06 20:57:01 +0200

    lesson day 8

--- Day008/lesson_day8.py
+++ Day008/lesson_day8.py
@@ -28,3 +28,10 @@ greet_with("nowhere", "Philipp")
 
 
 greet_with(location="nowhere", name="Philipp")
+
+
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+temp = alphabet.index['a'"]
+print(temp)
