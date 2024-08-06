65b43fb348fda914736c35bbe74b2282939b7688
Eisi <philipp.eisenmann@googlemail.com>
2024-08-06 21:12:42 +0200

    ceasar decode

--- Day008/ceasar_cipher_2.py
+++ Day008/ceasar_cipher_2.py
@@ -12,20 +12,24 @@ shift = int(input("Type the shift number:\n"))
 #  Use the value of the user chosen 'direction' variable to determine which functionality to use.
 
 def ceasar(original_text, shift_amount, encode_or_decode):
-    cipher_text = ""
+    output_text = ""
 
     if encode_or_decode == "decode":
         shift_amount *= -1
 
 
     for letter in original_text:
-        shifted_position = alphabet.index(letter) + shift_amount
-        shifted_position %= len(alphabet)
-        cipher_text += alphabet[shifted_position]
+
+        if letter not in alphabet:
+            output_text += letter
+        else:
+            shifted_position = alphabet.index(letter) + shift_amount
+            shifted_position %= len(alphabet)
+            output_text += alphabet[shifted_position]
 
 
 
-    print(f"Here is the encoded result: {cipher_text}")    
+    print(f"Here is the encoded result: {output_text}")    
 
 
         
